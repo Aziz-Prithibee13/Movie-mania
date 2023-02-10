f380ac7953775772f5e7561a3c2ee07b30d4675b
Prithibee13 <azizulkarim97@gmail.com>
2023-02-10 19:50:42 +0600

    first-commit

---
+++ Components/Movies/Movies.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View } from 'react-native';
+
+const Movies = () => {
+    return (
+        <View>
+            
+        </View>
+    );
+};
+
+export default Movies;
